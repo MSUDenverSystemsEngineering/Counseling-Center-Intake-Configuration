aa6b2c443ea56c31fdb3d0b829a6cf399a69ce9f
Steve Patterson <spatte30@msudenver.edu>
2018-05-24 10:12:35 -0600

    Add change to disable add-ons warning

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -147,7 +147,11 @@ Try {
 		Execute-Process -Path “IE11-Setup-Branding.exe” -Parameters ‘/q’
 		Set-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableTaskMgr" -Value 1 -Type DWORD
 		Set-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableChangePassword" -Value 1 -Type DWORD
+		Set-RegistryKey -Key "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableChangePassword" -Value 1 -Type DWORD
+		Set-RegistryKey -Key "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Ext" -Name "IgnoreFrameApprovalCheck" -Value 1 -Type DWORD
+		Set-RegistryKey -Key "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\Ext" -Name "IgnoreFrameApprovalCheck" -Value 1 -Type DWORD
 
+HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\
 		Dism /online /Enable-Feature /all /FeatureName:Client-EmbeddedShellLauncher /quiet /norestart
 
 		# Check if shell launcher license is enabled
